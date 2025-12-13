79487ba7cb96f1b1d1bd9a6c1cc444bb430049d0
Mitchell Vuong <m.hf.vuong@gmail.com>
2025-12-13 22:29:05 +0100

    Day 6 part 2 finished

--- AoC_2025/day_06.rs
+++ AoC_2025/day_06.rs
@@ -1,9 +1,7 @@
 use std::fs;
-// use std::str;
 
 fn part_01(problems: &Vec<&str>) -> usize {
     let problem_amount = problems[0].split_whitespace().collect::<Vec<_>>().len();
-    // println!("{:?}", problems[0].split_whitespace().collect::<Vec<_>>());
     let mut answers = vec![0; problem_amount];
     let addition_multiplication: Vec<&str> = problems[problems.len() - 1].split_whitespace().collect();
     for p in 0..problems.len() - 1{
@@ -22,13 +20,48 @@ fn part_01(problems: &Vec<&str>) -> usize {
     return answers.iter().sum::<usize>();
 }
 
+fn part_02(problems: &Vec<&str>) -> usize {
+    let mut addition_multiplication: Vec<&str> = problems[problems.len() - 1].split_whitespace().collect();
+    let problem_amount = problems[0].split_whitespace().collect::<Vec<_>>().len();
+    let mut answers = vec![0; problem_amount];
+    let mut answers_idx = 0;
+    addition_multiplication.reverse();
+    for char in (0..problems[0].chars().collect::<Vec<_>>().len()).rev(){
+        let mut white_spaces = true;
+        let mut num_text = String::from("");
+
+        for p in 0..problems.len() - 1 {
+            let line: Vec<_> = problems[p].chars().collect();
+            if line[char] != ' '{
+                white_spaces = false;
+                num_text.push_str(&line[char].to_string());
+            }
+        }
+        if white_spaces == true{
+            answers_idx += 1;
+        } else {
+            let number = num_text.parse::<usize>().unwrap();
+            if answers[answers_idx] == 0{
+                answers[answers_idx] = number;
+            } else {
+                if addition_multiplication[answers_idx] == "+"{
+                    answers[answers_idx] += number;
+                } else if addition_multiplication[answers_idx] == "*" {
+                    answers[answers_idx] *= number;
+                }
+            }
+        }
+    }
+    return answers.iter().sum::<usize>();
+}
+
 fn main(){
     let data = fs::read_to_string("day_06.txt").expect("Unable to read");
     let lines = data.lines();
     let mut problems: Vec<&str> = Vec::new();
     for line in lines {
-        problems.push(line.trim());
+        problems.push(line);
     }
-    // println!("{:?}", problems);
     println!("Part 1: {}", part_01(&problems));
+    println!("Part 2: {}", part_02(&problems));
 }
